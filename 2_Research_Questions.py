74f82b00943efd638882510d2be246516585658e
nouraalsayed05 <alsayednoura9@gmail.com>
2025-12-08 12:24:03 +0100

    Update 2_Research_Questions.py

--- 2_Research_Questions.py
+++ 2_Research_Questions.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Covid Data Analysis - Research Questions",
+    page_title="Cardiovascular disease - Research Questions",
     page_icon="📊",
     #layout="wide"   
 )
@@ -45,12 +45,12 @@ st.sidebar.image("Ulogo.png")
 st.title("Research Questions")
 st.divider()
 
-st.subheader("1.Is there a significant difference in COVID-19 case counts between different age groups?")
+st.subheader("Can changes in pulse pressure between Visit 1 and Visit 2 predict the occurrence of a CVD event before Visit 3 in Framingham participants?")
 st.markdown("""- **H0**: The distribution of COVID-19 cases across age groups is proportional to the population distribution of those age groups, indicating that age, relative to its population size, does not influence the likelihood of contracting COVID-19.
 - **H1**: The distribution of COVID-19 cases across age groups is not proportional to the population distribution of those age groups, suggesting that, relative to their population size, certain age groups are more likely to contract COVID-19 than others.
 """)
 
-st.subheader("2. Do gender, age group, and case year significantly associate with COVID-19 mortality outcomes?")
+st.subheader("2. Is the association between ΔPP and CVD different for women vs men?")
 st.markdown("""- **H0**: Gender, age group, and case year do not significantly predict COVID-19 mortality.
 - **H1**: Gender, age group, and case year significantly predict COVID-19 mortality.""")
 
